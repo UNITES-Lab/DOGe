30ac033613f65d7237bf0b71b50e15f0c683cfca
Pingzhi Li <pingzhi@cs.unc.edu>
2025-06-19 21:43:34 -0700

    init

---
+++ setup.sh
@@ -0,0 +1,6 @@
+ conda create -y -n antidistill python=3.10
+ conda activate antidistill
+ pip install -r requirements.txt
+
+mkdir -p data
+#huggingface-cli download --repo-type dataset ANONYMOUS/antidistill-exps --local-dir data/antidistill-exps --token $HF_TOKEN
